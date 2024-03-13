fe5d5471e5b586e2c1014822ae6b502715c72530
RaoMujtaba <mujtabarao99@gmail.com>
2024-03-13 14:13:49 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.mediaplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button play;
+    private Button pause;
+    private SeekBar seekBar;
+    private MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        play = findViewById(R.id.play);
+        pause = findViewById(R.id.pause);
+        seekBar = findViewById(R.id.seekBar);
+
+        //Media Player using local source
+        mediaPlayer = MediaPlayer.create(this, R.raw.mustafa_ghulamon_ki_kismat_badalte_hain);
+
+        /*
+         Media player use remote source
+         mediaPlayer = new mediaPlayer();
+         try {
+          mediaPlayer.setDataSource("Link");
+         } catch (IOException e){
+          e.printStackTrace();
+          }
+          //write in above application tag in androidManifest.xml is uses as grant permission
+          <uses-permission android:name="android.permission.INTERNET" />
+          //write in application tag is used to clear traffic
+          android:usesCleartextTraffic="true"
+        
+        // run the media file
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                Toast.makeText(MainActivity.this,"Ready to play",Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+                seekBar.setMax(mediaPlayer.getDuration());
+                seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                        if(b)
+                        {
+                            mediaPlayer.seekTo(i);
+                        }
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                });
+            }
+        });
+        //Prepare the media file
+        mediaPlayer.prepareAsync();
+        */
+        mediaPlayer.start();
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
